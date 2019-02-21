6d13a04cfb70af9779df96a9b9839de2da2870a6
drnasmith <neil.smith@diamond.ac.uk>
2019-02-21 15:33:45 +0000

    Changed feedback form to use vue with vee-validate

--- client/js/modules/feedback/views/feedback.js
+++ client/js/modules/feedback/views/feedback.js
@@ -1,28 +1,71 @@
-define(['marionette',
-    'modules/feedback/models/feedback',
-    'views/form',
-    'tpl!templates/feedback/feedback.html',
-    ], function(Marionette, Feedback, FormView, template) {
-    
-    return FormView.extend({
-        template: template,
-        
-        createModel: function() {
-            this.model = new Feedback()
-        },
-        
-        success: function() {
-            app.alert({ message: 'Feedback successfully submitted' })
-            
-            _.each(['name', 'email', 'feedback'], function(f) {
-                this.$el.find('[name='+f+']').val('')
-            }, this)
-            
-        },
+define(['utils/marionette_vuewrapper',
+        'vue',
+        'vee-validate',
+        'modules/feedback/models/feedback',
+        'text!templates/feedback/feedback.html',
+        ], function(VueWrapper, Vue, VeeValidate, FeedbackModel, tmpl) {
 
-        failure: function() {
-            app.alert({ message: 'Something went wrong submitting this feedback, please try again' })
-        },
+    // Declare use of validation plugin
+    Vue.use(VeeValidate)
+
+    return VueWrapper.extend({
+        vueView: Vue.extend({
+            template: tmpl,
+
+            // Data needs to be a function as we are defining a Vue component here
+            data: function() {
+                return {
+                    // Form elements
+                    name: null,
+                    email: null,
+                    feedback: null,
+                    // Control for loading spinner
+                    isLoading: false
+                }
+            },
+
+            methods: {
+                onSubmit(event) {
+                    event.preventDefault()
+                    let self = this // Save this for resetting if submit is ok
+
+                    // Check vee-validation passes
+                    this.$validator.validateAll().then((result) => {
+                        if (result) {
+                            // Set the attribute to show loading spinner
+                            self.isLoading = true
+                
+                            let model = new FeedbackModel({
+                                name: this.name,
+                                email: this.email,
+                                feedback: this.feedback
+                            })
+                            // Save model (post) to server
+                            model.save({}, {
+                                success: function(model, response, options) {
+                                    // Reset all data to defaults
+                                    self.reset()
+                                    app.alert({message: "Your feedback has been submitted!", notify: true})
+                                    self.isLoading = false
+                                },
+                                error: function(model, response, options) {
+                                    app.alert({message: "Your feedback has NOT been submitted - Failed validation on server!"})
+                                    self.isLoading = false
+                                }})
+                        } else {
+                            console.log('Form validation failed - UI should show errors!')
+                        }
+                    });
+                },
+                // Called on successful submission
+                reset: function() {
+                    this.name = null
+                    this.email = null
+                    this.feedback = null
+                    // If we don't call reset form will show errors straight away
+                    this.$validator.reset()
+                }
+            },
+        })
     })
-        
 })
